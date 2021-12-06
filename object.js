60b7d75bbde54c5292159f1f1440a0909a1d1b46
sagar <sagarkhadka2069@gmail.com>
2021-12-06 23:10:09 +0545

    Factory function and Constructor function

--- object.js
+++ object.js
@@ -1,10 +1,10 @@
 //The Simplest way to create an boject is using an "object literal"
-const circle = {
-    radius: 1,
-    draw() {
-        console.log("Circle");
-    },
-};
+// const circle = {
+//     radius: 1,
+//     draw() {
+//         console.log("Circle");
+//     },
+// };
 
 //To create multiple objects with the same structure and behaviour(methods), use a "factory" or a "constructor".
 
@@ -12,12 +12,25 @@ const circle = {
 function createCircle(radius) {
     return {
         radius,
-        draw: function () {},
+        draw: function () {
+            console.log("Draw");
+        },
     };
 }
+const circle = createCircle(2);
 
 //Constructor function
 function Circle(radius) {
+    console.log("this", this);
     this.radius = radius;
     this.draw = function () {};
 }
+
+const smallCircle = new Circle(3);
+
+//Value vs Reference Types
+
+//Primitives are copied by their value
+///Example======>Number,String,Boolean,Symbol,undefined,null
+//Objects are copied by their reference
+///Example======>Object,Function,Array
