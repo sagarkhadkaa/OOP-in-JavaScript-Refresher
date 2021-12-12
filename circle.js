316c7fa528a1692a251c0026e65711ea349c286c
sagar <sagarkhadka2069@gmail.com>
2021-12-12 17:46:26 +0545

    OOP wrapup

---
+++ circle.js
@@ -0,0 +1,12 @@
+//Implementation details
+const _radius = new WeakMap();
+
+//Public Interface
+export class Circle {
+    constructor(radius) {
+        _radius.set(this, radius);
+    }
+    draw() {
+        console.log("Circle with radius " + _radius.get(this));
+    }
+}
--- index.js
+++ index.js
@@ -129,25 +129,30 @@
 //Polymorphism Problem/Solution
 
 //Stack Implementation
-const _items = new WeakMap();
-class Stack {
-    constructor() {
-        _items.set(this, []);
-    }
-    push(obj) {
-        _items.get(this).push(obj);
-    }
-    pop() {
-        const items = _items.get(this);
-        if (items.length === 0) throw new Error("Stack is empty");
-        return items.pop();
-    }
-    peek() {
-        const items = _items.get(this);
-        if (items.length === 0) throw new Error("Stack is empty");
-        return items[items.length - 1];
-    }
-    get count() {
-        return _items.get(this).length;
-    }
-}
+// const _items = new WeakMap();
+// class Stack {
+//     constructor() {
+//         _items.set(this, []);
+//     }
+//     push(obj) {
+//         _items.get(this).push(obj);
+//     }
+//     pop() {
+//         const items = _items.get(this);
+//         if (items.length === 0) throw new Error("Stack is empty");
+//          return items.pop();
+//     }
+//     peek() {
+//         const items = _items.get(this);
+//         if (items.length === 0) throw new Error("Stack is empty");
+//         return items[items.length - 1];
+//     }
+//     get count() {
+//         return _items.get(this).length;
+//     }
+// }
+
+import { Circle } from "./circle.js";
+
+const c = new Circle(20);
+c.draw();
